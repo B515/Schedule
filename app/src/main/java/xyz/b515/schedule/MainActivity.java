a6942342976e9fe047245e63cc59c877db9d7588
ZeroGoYoosee <qz777zp@live.cn>
2017-02-14 21:38:23 +0800

    Add AddActivity
    Add AddActivity

--- app/src/main/java/xyz/b515/schedule/MainActivity.java
+++ app/src/main/java/xyz/b515/schedule/MainActivity.java
@@ -1,5 +1,6 @@
 package xyz.b515.schedule;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -41,11 +42,19 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        switch(id){
+            case R.id.action_add: {
+                Intent intent=new Intent(this, AddActivity.class);
+                startActivity(intent);
+            } break;
+            case R.id.action_refresh: {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            } break;
+            case R.id.action_settings: {
+
+            } break;
         }
+        //noinspection SimplifiableIfStatement
 
         return super.onOptionsItemSelected(item);
     }
