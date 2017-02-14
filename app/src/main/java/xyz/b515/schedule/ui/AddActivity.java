97e0a97434242a82090b743064fa4fd90c6c62ac
ZeroGoYoosee <qz777zp@live.cn>
2017-02-14 21:39:47 +0800

    Move
    Move activities to /ui

--- app/src/main/java/xyz/b515/schedule/ui/AddActivity.java
+++ app/src/main/java/xyz/b515/schedule/ui/AddActivity.java
@@ -1,4 +1,4 @@
-package xyz.b515.schedule;
+package xyz.b515.schedule.ui;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+
+import xyz.b515.schedule.R;
 
 import static xyz.b515.schedule.R.id.toolbar;
 
--- app/src/main/java/xyz/b515/schedule/ui/MainActivity.java
+++ app/src/main/java/xyz/b515/schedule/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package xyz.b515.schedule;
+package xyz.b515.schedule.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import xyz.b515.schedule.R;
 
 public class MainActivity extends AppCompatActivity {
 
