19e55dce31a25f675995fbe93acfdb4af7fb8feb
Yun Liu <liuyun.97@gmail.com>
2017-02-14 21:19:29 +0800

    Getter Setter

--- app/src/main/java/xyz/b515/schedule/entity/Course.java
+++ app/src/main/java/xyz/b515/schedule/entity/Course.java
@@ -33,4 +33,92 @@ public class Course {
         this.oddWeek = oddWeek;
         this.evenWeek = evenWeek;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public int getStartWeek() {
+        return startWeek;
+    }
+
+    public int getEndWeek() {
+        return endWeek;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public boolean isOddWeek() {
+        return oddWeek;
+    }
+
+    public boolean isEvenWeek() {
+        return evenWeek;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public void setStartWeek(int startWeek) {
+        this.startWeek = startWeek;
+    }
+
+    public void setEndWeek(int endWeek) {
+        this.endWeek = endWeek;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setOddWeek(boolean oddWeek) {
+        this.oddWeek = oddWeek;
+    }
+
+    public void setEvenWeek(boolean evenWeek) {
+        this.evenWeek = evenWeek;
+    }
 }
