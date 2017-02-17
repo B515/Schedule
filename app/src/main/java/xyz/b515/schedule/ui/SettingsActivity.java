f3e80ecfd1125aedcb03f1f0cfd895ed73e31d90
ZeroGoYoosee <qz777zp@live.cn>
2017-02-17 18:57:50 +0800

    Add Settings & upgrade version
    Add Settings & upgrade version

---
+++ app/src/main/java/xyz/b515/schedule/ui/SettingsActivity.java
@@ -0,0 +1,17 @@
+package xyz.b515.schedule.ui;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by ZeroGo on 2017.2.16.
+ */
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
