b62d2757ff173ac51dfe2e9f537189b5c01d69b8
Yun Liu <liuyun.97@gmail.com>
2017-05-24 19:30:25 +0800

    Fix

--- app/src/main/java/xyz/b515/schedule/util/PackageHelper.java
+++ app/src/main/java/xyz/b515/schedule/util/PackageHelper.java
@@ -7,7 +7,7 @@ import android.content.pm.PackageManager;
 public class PackageHelper {
     public static void changeMain(Context context, boolean splash) {
         ComponentName splashActivity = new ComponentName(context, "xyz.b515.schedule.ui.view.SplashActivity");
-        ComponentName mainActivity = new ComponentName(context, "xyz.b515.schedule.ui.MainActivityAlias");
+        ComponentName mainActivity = new ComponentName(context, "xyz.b515.schedule.ui.view.MainActivityAlias");
 
         PackageManager manager = context.getPackageManager();
         if (splash) {
