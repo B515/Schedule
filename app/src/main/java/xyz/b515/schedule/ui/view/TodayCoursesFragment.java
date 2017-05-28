9a2a1cfa6e2c543b94372a6096200fdb97871d05
Yun Liu <liuyun.97@gmail.com>
2017-05-28 17:24:40 +0800

    We don't need WildHunter anymore.

--- app/src/main/java/xyz/b515/schedule/ui/view/TodayCoursesFragment.java
+++ app/src/main/java/xyz/b515/schedule/ui/view/TodayCoursesFragment.java
@@ -1,6 +1,8 @@
 package xyz.b515.schedule.ui.view;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -17,10 +19,10 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import java8.util.stream.StreamSupport;
+import xyz.b515.schedule.Constant;
 import xyz.b515.schedule.R;
 import xyz.b515.schedule.db.CourseManager;
 import xyz.b515.schedule.entity.Course;
-import xyz.b515.schedule.entity.Spacetime;
 import xyz.b515.schedule.ui.adapter.CourseAdapter;
 
 /**
@@ -32,6 +34,7 @@ public class TodayCoursesFragment extends Fragment {
     @BindView(R.id.recycler) RecyclerView recycler;
     CourseAdapter adapter;
     CourseManager manager;
+    SharedPreferences preferences;
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
@@ -40,6 +43,7 @@ public class TodayCoursesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_today, container, false);
         ButterKnife.bind(this, view);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
@@ -49,6 +53,7 @@ public class TodayCoursesFragment extends Fragment {
         recycler.setItemAnimator(new DefaultItemAnimator());
 
         int today = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+        int currentWeek = preferences.getInt(Constant.CURRENT_WEEK, -1);
         adapter.items.clear();
         manager = new CourseManager(getContext());
         List<Course> list = manager.getAllCourse();
@@ -56,6 +61,7 @@ public class TodayCoursesFragment extends Fragment {
             StreamSupport.stream(list)
                     .flatMap(course -> StreamSupport.stream(course.getSpacetimes()))
                     .filter(spacetime -> spacetime.getWeekday() == today)
+                    .filter(spacetime -> spacetime.getStartWeek() <= currentWeek && spacetime.getEndWeek() >= currentWeek)
                     .forEach(spacetime -> adapter.items.add(spacetime.getCourse()));
         }
         adapter.notifyDataSetChanged();
