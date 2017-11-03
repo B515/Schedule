4cd8c76e95c52a799b71d7c8668f4977c4b6e719
Yun <liuyun.97@gmail.com>
2017-11-03 16:39:30 +0800

    Fix parsing

--- app/src/main/kotlin/xyz/b515/schedule/util/CourseParser.kt
+++ app/src/main/kotlin/xyz/b515/schedule/util/CourseParser.kt
@@ -6,10 +6,9 @@ import xyz.b515.schedule.db.CourseManager
 import xyz.b515.schedule.entity.Course
 import xyz.b515.schedule.entity.Spacetime
 import java.util.*
-import java.util.regex.Pattern
 
 object CourseParser {
-    private val weekdayTimePattern = Pattern.compile("周(.)第(.+)节\\{第(\\d+)-(\\d+)周\\}")
+    private val weekdayTimePattern = "周(.)第(.+)节\\{第(\\d+)-(\\d+)周\\}".toRegex()
 
     fun parse(text: String, manager: CourseManager) {
         val names = ArrayList<String>()
@@ -17,49 +16,48 @@ object CourseParser {
         val document = Jsoup.parse(text)
         val courses = document.getElementById("Table1").getElementsByTag("td")
         for (td in courses) {
-            if (!td.text().contains("周"))
+            if ("周" !in td.text())
                 continue
-            val nodes = td.textNodes()
 
-            val name = nodes[0].text()
-            val course: Course?
-            if (!names.contains(name)) {
-                names.add(name)
+            for (nodes in td.textNodes().map { it.text() }.chunk(4)) {
+                val name = nodes[0]
+                val course: Course
+                if (name !in names) {
+                    names.add(name)
 
-                course = Course()
-                course.name = name
-                course.teacher = nodes[2].text()
+                    course = Course()
+                    course.name = name
+                    course.teacher = nodes[2]
 
-                val color = ColorPickerDialog.MATERIAL_COLORS[Random().nextInt(18)]
-                course.color = ColorHelper.shadeColor(color, 0.33)
+                    val color = ColorPickerDialog.MATERIAL_COLORS[Random().nextInt(18)]
+                    course.color = ColorHelper.shadeColor(color, 0.33)
 
-                manager.insertCourse(course)
-            } else {
-                course = manager.getCourse(name)
+                    manager.insertCourse(course)
+                } else {
+                    course = manager.getCourse(name)
+                }
+
+                val spacetime = Spacetime()
+                spacetime.course = course
+
+                val m = weekdayTimePattern.find(nodes[1])
+                m?.let {
+                    val values = m.groupValues
+                    val weeks = values[2].split(",")
+                    with(spacetime) {
+                        weekday = translateWeekday(values[1])
+                        startTime = weeks.first().toInt()
+                        endTime = weeks.last().toInt()
+                        startWeek = values[3].toInt()
+                        endWeek = values[4].toInt()
+                    }
+                }
+                spacetime.location = nodes[3]
+                spacetime.oddWeek = "单周" in nodes[1]
+                spacetime.evenWeek = "双周" in nodes[1]
+
+                manager.insertSpacetime(spacetime)
             }
-
-            val spacetime = Spacetime()
-            spacetime.course = course
-
-            val m = weekdayTimePattern.matcher(nodes[1].text())
-            if (m.find()) {
-                spacetime.weekday = translateWeekday(m.group(1))
-                val weeks = m.group(2).split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-                spacetime.startTime = Integer.parseInt(weeks[0])
-                spacetime.endTime = Integer.parseInt(weeks[weeks.size - 1])
-                spacetime.startWeek = Integer.parseInt(m.group(3))
-                spacetime.endWeek = Integer.parseInt(m.group(4))
-            }
-            //TODO 数据通信原理 {第1-16周|2节/周}
-
-            spacetime.location = nodes[3].text()
-            if (text.contains("单周")) {
-                spacetime.oddWeek = true
-            } else if (text.contains("双周")) {
-                spacetime.evenWeek = true
-            }
-
-            manager.insertSpacetime(spacetime)
         }
     }
 
@@ -72,5 +70,13 @@ object CourseParser {
         "六" -> Calendar.SATURDAY
         "日" -> Calendar.SUNDAY
         else -> 0
+    }
+
+    fun <T> List<T>.chunk(size: Int): List<List<T>> {
+        tailrec fun <T> List<T>._chunk(size: Int, initial: List<List<T>> = emptyList()): List<List<T>> {
+            val result = initial + listOf(this.take(size))
+            return if (this.size > size) this.drop(size)._chunk(size, result) else result
+        }
+        return this._chunk(size)
     }
 }
